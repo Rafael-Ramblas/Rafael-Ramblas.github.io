0647f4da9cffcf1b81d2c5ed498c71a129594a67
Rafael <rafaelmramblas@gmail.com>
2019-02-16 16:12:45 -0200

    Updates

---
+++ precache-manifest.dee2c4fea39bf9aadd0617bd3114b93a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d632408ea32b792f6215",
+    "url": "/static/js/main.d632408e.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "d632408ea32b792f6215",
+    "url": "/static/css/main.0d8ebdf3.chunk.css"
+  },
+  {
+    "revision": "b59f4911172d8a8b5d4b512ca059a437",
+    "url": "/index.html"
+  }
+];
